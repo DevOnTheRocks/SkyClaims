b97b452b59e09731e81e623e1185f16d08844da1
Mohron <mohron@mohron.net>
2017-01-01 04:43:00 -0600

    Added more info and functionality to the info command

--- src/main/java/net/mohron/skyclaims/command/CommandInfo.java
+++ src/main/java/net/mohron/skyclaims/command/CommandInfo.java
@@ -1,10 +1,12 @@
 package net.mohron.skyclaims.command;
 
+import me.ryanhamshire.griefprevention.command.CommandHelper;
 import net.mohron.skyclaims.SkyClaims;
 import net.mohron.skyclaims.island.Island;
 import net.mohron.skyclaims.lib.Arguments;
 import net.mohron.skyclaims.lib.Permissions;
 import net.mohron.skyclaims.util.IslandUtil;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandPermissionException;
 import org.spongepowered.api.command.CommandResult;
@@ -17,9 +19,11 @@ import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.User;
 import org.spongepowered.api.service.pagination.PaginationList;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.action.TextActions;
 import org.spongepowered.api.text.format.TextColors;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 public class CommandInfo implements CommandExecutor {
 
@@ -63,14 +67,40 @@ public class CommandInfo implements CommandExecutor {
 			Text infoText = Text.of(
 					TextColors.YELLOW, "Owner", TextColors.WHITE, " : ", TextColors.GRAY, island.getOwnerName(), "\n",
 					TextColors.YELLOW, "Size", TextColors.WHITE, " : ", TextColors.GRAY, island.getRadius() * 2, "x", island.getRadius() * 2, "\n",
-					TextColors.YELLOW, "Spawn", TextColors.WHITE, " : ", TextColors.LIGHT_PURPLE, island.getSpawn().getBlockX(), TextColors.GRAY, " x, ", TextColors.LIGHT_PURPLE, island.getSpawn().getBlockY(), TextColors.GRAY, " y, ", TextColors.LIGHT_PURPLE, island.getSpawn().getBlockZ(), TextColors.GRAY, " z", "\n",
-					TextColors.YELLOW, "Claim", TextColors.WHITE, " : ", TextColors.GRAY, island.getClaim().getID()
+					TextColors.YELLOW, "Spawn", TextColors.WHITE, " : ", TextColors.LIGHT_PURPLE, island.getSpawn().getBlockX(), TextColors.GRAY, " x, ",
+					TextColors.LIGHT_PURPLE, island.getSpawn().getBlockY(), TextColors.GRAY, " y, ", TextColors.LIGHT_PURPLE, island.getSpawn().getBlockZ(), TextColors.GRAY, " z", "\n",
+					TextColors.YELLOW, "Claim", TextColors.WHITE, " : ", TextColors.GRAY, Text.builder(island.getClaim().getID().toString())
+							.onClick(TextActions.executeCallback(createCommandConsumer(src, "claiminfo", island.getClaim().getID().toString(), createReturnIslandInfoConsumer(src, ""))))
+							.onHover(TextActions.showText(Text.of("Click here to check claim info."))),
+					TextColors.YELLOW, "Created", TextColors.WHITE, " : ", TextColors.GRAY, island.getDateCreated()
 			);
 
-			PaginationList.Builder paginationBuilder = PaginationList.builder().title(Text.of(TextColors.AQUA, "Island Info")).padding(Text.of("-")).contents(infoText);
+			PaginationList.Builder paginationBuilder = PaginationList.builder().title(Text.of(TextColors.AQUA, "Island Info")).padding(Text.of(TextColors.AQUA, "-")).contents(infoText);
 			paginationBuilder.sendTo(src);
 
 			return CommandResult.success();
 		}
+	}
+
+	private static Consumer<CommandSource> createCommandConsumer(CommandSource src, String command, String arguments, Consumer<CommandSource> postConsumerTask) {
+		return consumer -> {
+			try {
+				Sponge.getCommandManager().get(command).get().getCallable().process(src, arguments);
+			} catch (CommandException e) {
+				src.sendMessage(e.getText());
+			}
+			if (postConsumerTask != null) {
+				postConsumerTask.accept(src);
+			}
+		};
+	}
+
+	private Consumer<CommandSource> createReturnIslandInfoConsumer(CommandSource src, String arguments) {
+		return consumer -> {
+			Text claimListReturnCommand = Text.builder().append(Text.of(
+					TextColors.WHITE, "\n[", TextColors.AQUA, "Return to island info", TextColors.WHITE, "]\n"))
+					.onClick(TextActions.executeCallback(CommandHelper.createCommandConsumer(src, "is info", arguments))).build();
+			src.sendMessage(claimListReturnCommand);
+		};
 	}
 }
