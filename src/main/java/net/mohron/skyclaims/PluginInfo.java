767533beacba853e8f8a70a6f85a53148ce8f6ef
Slind14 <slind014@gmail.com>
2017-01-01 19:01:40 +0100

    revert VERSION back to 0.0.1 (sorry that I messed up your versioning Mohron)

--- src/main/java/net/mohron/skyclaims/PluginInfo.java
+++ src/main/java/net/mohron/skyclaims/PluginInfo.java
@@ -3,7 +3,7 @@ package net.mohron.skyclaims;
 public class PluginInfo {
 	public static final String ID = "skyclaims";
 	public static final String NAME = "SkyClaims";
-	public static final String VERSION = "0.0.2";
+	public static final String VERSION = "0.0.1";
 	public static final String DESCRIPTION = "SkyClaims is an Island plugin that integrates with GriefPrevention for Sponge servers.";
 	public static final String AUTHORS = "Mohron, Cossacksman, Slind";
 }
