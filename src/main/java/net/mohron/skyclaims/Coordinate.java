0fb9e5b5c557e0800d4ec9638e56f242cfe6c1db
Mohron <mohron@mohron.net>
2017-01-01 22:37:20 -0600

    Added Coordinate and Region boundary getters

---
+++ src/main/java/net/mohron/skyclaims/Coordinate.java
@@ -0,0 +1,19 @@
+package net.mohron.skyclaims;
+
+public class Coordinate {
+	public int x;
+	public int z;
+
+	public Coordinate(int x, int z) {
+		this.x = x;
+		this.z = z;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getZ() {
+		return z;
+	}
+}
--- src/main/java/net/mohron/skyclaims/Region.java
+++ src/main/java/net/mohron/skyclaims/Region.java
@@ -1,22 +1,27 @@
 package net.mohron.skyclaims;
 
-/**
- * Created by cossacksman on 02/01/17.
- */
 public class Region {
-    public int x;
-    public int z;
+	public int x;
+	public int z;
 
-    public Region(int x, int z) {
-        this.x = x;
-        this.z = z;
-    }
+	public Region(int x, int z) {
+		this.x = x;
+		this.z = z;
+	}
 
-    public int getX() {
-        return x;
-    }
+	public int getX() {
+		return x;
+	}
 
-    public int getZ() {
-        return z;
-    }
+	public int getZ() {
+		return z;
+	}
+
+	public Coordinate getLesserBoundary() {
+		return new Coordinate(x << 5 << 4, z << 5 << 4);
+	}
+
+	public Coordinate getGreaterBoundary() {
+		return new Coordinate((((x + 1) << 5) << 4) - 1, (((z + 1) << 5) << 4) - 1);
+	}
 }
