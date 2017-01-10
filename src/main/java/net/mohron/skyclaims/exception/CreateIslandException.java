a938ec46b563c4345017164077450c41c499fb41
Mohron <mohron@mohron.net>
2017-01-09 20:47:00 -0600

    Created Plugin Exception Class

--- src/main/java/net/mohron/skyclaims/exception/CreateIslandException.java
+++ src/main/java/net/mohron/skyclaims/exception/CreateIslandException.java
@@ -2,15 +2,8 @@ package net.mohron.skyclaims.exception;
 
 import org.spongepowered.api.text.Text;
 
-public class CreateIslandException extends Exception {
-	private Text message;
-
+public class CreateIslandException extends SkyClaimsException {
 	public CreateIslandException(Text message) {
-		super(message.toPlain());
-		this.message = message;
-	}
-
-	public Text getText() {
-		return message;
+		super(message);
 	}
 }
--- src/main/java/net/mohron/skyclaims/exception/InvalidRegionException.java
+++ src/main/java/net/mohron/skyclaims/exception/InvalidRegionException.java
@@ -2,15 +2,8 @@ package net.mohron.skyclaims.exception;
 
 import org.spongepowered.api.text.Text;
 
-public class InvalidRegionException extends Exception {
-	private Text message;
-
+public class InvalidRegionException extends SkyClaimsException {
 	public InvalidRegionException(Text message) {
-		super(message.toPlain());
-		this.message = message;
-	}
-
-	public Text getText() {
-		return message;
+		super(message);
 	}
 }
