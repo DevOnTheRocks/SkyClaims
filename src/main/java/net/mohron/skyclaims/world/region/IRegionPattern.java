aeb96713347a99eded1276bb2832c8a279eb9ec1
adam_ <adam_sanchez@corruptiongames.net>
2017-01-15 08:41:38 -0800

    Add lastLocation method to interface for use in algorithms

--- src/main/java/net/mohron/skyclaims/world/region/IRegionPattern.java
+++ src/main/java/net/mohron/skyclaims/world/region/IRegionPattern.java
@@ -1,6 +1,7 @@
 package net.mohron.skyclaims.world.region;
 
 import net.mohron.skyclaims.exception.InvalidRegionException;
+import net.mohron.skyclaims.world.Coordinate;
 
 import java.util.ArrayList;
 
@@ -8,4 +9,6 @@ public interface IRegionPattern {
 	ArrayList<Region> generateRegionPattern();
 
 	public Region nextRegion() throws InvalidRegionException;
+
+	public Coordinate lastLocation();
 }
