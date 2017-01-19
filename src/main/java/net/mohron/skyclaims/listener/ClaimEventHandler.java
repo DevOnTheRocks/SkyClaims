889a6dd6948ca3b26e259c0e47ade3b749cf1c2d
Mohron <mohron@mohron.net>
2017-01-19 00:00:01 -0600

    Remove unneded checks. Fixed create listener

--- src/main/java/net/mohron/skyclaims/listener/ClaimEventHandler.java
+++ src/main/java/net/mohron/skyclaims/listener/ClaimEventHandler.java
@@ -6,7 +6,6 @@ import me.ryanhamshire.griefprevention.api.event.DeleteClaimEvent;
 import me.ryanhamshire.griefprevention.api.event.ResizeClaimEvent;
 import net.mohron.skyclaims.SkyClaims;
 import net.mohron.skyclaims.util.ConfigUtil;
-import net.mohron.skyclaims.util.WorldUtil;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.filter.cause.Root;
@@ -17,7 +16,7 @@ public class ClaimEventHandler {
 	@Listener
 	public void onClaimCreate(CreateClaimEvent event, @Root Player player) {
 		Claim claim = event.getClaim();
-		if (event.isCancelled() || (claim.getWorld() != ConfigUtil.getWorld() && claim.isBasicClaim())) return;
+		if (claim.getWorld() != ConfigUtil.getWorld() || !claim.isBasicClaim()) return;
 
 		player.sendMessage(Text.of(TextColors.RED, "You cannot create claims in this dimension!"));
 		event.setCancelled(true);
@@ -26,7 +25,7 @@ public class ClaimEventHandler {
 	@Listener
 	public void onClaimDelete(DeleteClaimEvent event, @Root Player player) {
 		Claim claim = event.getClaim();
-		if (event.isCancelled() || !SkyClaims.islandClaims.contains(claim)) return;
+		if (!SkyClaims.islandClaims.contains(claim)) return;
 
 		player.sendMessage(Text.of(TextColors.RED, "You cannot delete an island claim!"));
 		event.setCancelled(true);
@@ -35,7 +34,7 @@ public class ClaimEventHandler {
 	@Listener
 	public void onClaimResize(ResizeClaimEvent event, @Root Player player) {
 		Claim claim = event.getClaim();
-		if (event.isCancelled() || !SkyClaims.islandClaims.contains(claim)) return;
+		if (!SkyClaims.islandClaims.contains(claim)) return;
 
 		player.sendMessage(Text.of(TextColors.RED, "You cannot resize an island claim!"));
 		event.setCancelled(true);
