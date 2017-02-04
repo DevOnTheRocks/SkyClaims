cac15b4b864981f3b4118964f8523f76a6272cd8
Mohron <mohron@mohron.net>
2017-02-04 17:33:37 -0600

    Bump Version

--- src/main/java/net/mohron/skyclaims/PluginInfo.java
+++ src/main/java/net/mohron/skyclaims/PluginInfo.java
@@ -3,7 +3,7 @@ package net.mohron.skyclaims;
 public class PluginInfo {
 	public static final String ID = "skyclaims";
 	public static final String NAME = "SkyClaims";
-	public static final String VERSION = "1.0.0-beta13";
+	public static final String VERSION = "1.0.0-beta14";
 	public static final String DESCRIPTION = "SkyClaims is an Island Management plugin that integrates with GriefPrevention.";
 	public static final String AUTHORS = "Mohron, Cossacksman";
 	public static final double GP_API_VERSION = 0.2;
