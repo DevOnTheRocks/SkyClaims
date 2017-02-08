ffe98fccc07280f94ecb7dda7bad044837aa6e40
Mohron <mohron@mohron.net>
2017-02-07 21:08:09 -0600

    Added copyright file headers

--- src/main/java/net/mohron/skyclaims/database/SqliteDatabase.java
+++ src/main/java/net/mohron/skyclaims/database/SqliteDatabase.java
@@ -1,3 +1,21 @@
+/*
+ * SkyClaims - A Skyblock plugin made for Sponge
+ * Copyright (C) 2017 Mohron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SkyClaims is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SkyClaims.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.mohron.skyclaims.database;
 
 import com.flowpowered.math.vector.Vector3d;
