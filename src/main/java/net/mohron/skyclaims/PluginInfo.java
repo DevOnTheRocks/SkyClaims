c839b413c68c4f0ab6c775af5503d794cb5eb6ac
Mohron <mohron@mohron.net>
2017-02-13 01:08:23 -0600

    Added blossom gradle plugin

--- src/main/java/net/mohron/skyclaims/PluginInfo.java
+++ src/main/java/net/mohron/skyclaims/PluginInfo.java
@@ -20,9 +20,9 @@ package net.mohron.skyclaims;
 
 public class PluginInfo {
 	public static final String ID = "skyclaims";
-	public static final String NAME = "SkyClaims";
-	public static final String VERSION = "1.0.0-beta17";
-	public static final String DESCRIPTION = "SkyClaims is an Island Management plugin that integrates with GriefPrevention.";
+	public static final String NAME = "@NAME@";
+	public static final String VERSION = "@VERSION@";
+	public static final String DESCRIPTION = "@DESCRIPTION@";
 	public static final String AUTHORS = "Mohron, Cossacksman";
 	public static final double GP_API_VERSION = 0.2;
 }
