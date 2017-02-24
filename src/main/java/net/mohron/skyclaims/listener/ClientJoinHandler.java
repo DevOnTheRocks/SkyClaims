c13de01427824228d444317a7e74583a73744102
Mohron <mohron@mohron.net>
2017-02-23 19:42:41 -0600

    Add a short delay to creating an island on join

--- src/main/java/net/mohron/skyclaims/listener/ClientJoinHandler.java
+++ src/main/java/net/mohron/skyclaims/listener/ClientJoinHandler.java
@@ -22,23 +22,32 @@ import net.mohron.skyclaims.SkyClaims;
 import net.mohron.skyclaims.exception.CreateIslandException;
 import net.mohron.skyclaims.permissions.Options;
 import net.mohron.skyclaims.world.Island;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.filter.cause.Root;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
 
 public class ClientJoinHandler {
+	private static final SkyClaims PLUGIN = SkyClaims.getInstance();
+
 	@Listener
 	public void onClientJoin(ClientConnectionEvent.Join event, @Root Player player) {
-		if (!SkyClaims.getInstance().getConfig().getMiscConfig().createIslandOnJoin()) return;
+		if (!PLUGIN.getConfig().getMiscConfig().createIslandOnJoin() || Island.hasIsland(player.getUniqueId()))
+			return;
 
-		try {
-			if (!Island.hasIsland(player.getUniqueId()))
-				new Island(player, Options.getStringOption(player.getUniqueId(), Options.DEFAULT_SCHEMATIC));
-			SkyClaims.getInstance().getLogger().info(String.format("Automatically created an island for %s.", player.getName()));
-		} catch (CreateIslandException e) {
-			// Oh well, we tried!
-			SkyClaims.getInstance().getLogger().warn(String.format("Failed to create an island on join for %s.\n%s", player.getName(), e.getMessage()));
-		}
+		Sponge.getScheduler().createTaskBuilder()
+			.execute(src -> {
+				try {
+					new Island(player, Options.getStringOption(player.getUniqueId(), Options.DEFAULT_SCHEMATIC));
+					PLUGIN.getLogger().info(String.format("Automatically created an island for %s.", player.getName()));
+				} catch (CreateIslandException e) {
+					// Oh well, we tried!
+					PLUGIN.getLogger().warn(String.format("Failed to create an island on join for %s.\n%s", player.getName(), e.getMessage()));
+				}
+			})
+			.delayTicks(40)
+			.submit(PLUGIN);
+
 	}
 }
