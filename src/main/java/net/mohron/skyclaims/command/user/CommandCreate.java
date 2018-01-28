8a746f5b10fcb4233858d9eb17655c818b8f1c3a
Mohron <mohron@mohron.net>
2018-01-28 01:51:27 -0600

    Implemented `skyclaims.max-islands` option

--- src/main/java/net/mohron/skyclaims/command/user/CommandCreate.java
+++ src/main/java/net/mohron/skyclaims/command/user/CommandCreate.java
@@ -18,6 +18,10 @@
 
 package net.mohron.skyclaims.command.user;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import net.mohron.skyclaims.command.CommandBase;
 import net.mohron.skyclaims.command.CommandIsland;
 import net.mohron.skyclaims.command.argument.Arguments;
@@ -40,95 +44,96 @@ import org.spongepowered.api.text.format.TextColors;
 import org.spongepowered.api.text.format.TextStyles;
 import org.spongepowered.api.util.annotation.NonnullByDefault;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
-
 @NonnullByDefault
 public class CommandCreate extends CommandBase.PlayerCommand {
 
-    public static final String HELP_TEXT = "create an island.";
-    private static final Text SCHEMATIC = Text.of("schematic");
+  public static final String HELP_TEXT = "create an island.";
+  private static final Text SCHEMATIC = Text.of("schematic");
 
-    public static void register() {
-        CommandSpec commandSpec = CommandSpec.builder()
-            .permission(Permissions.COMMAND_CREATE)
-            .description(Text.of(HELP_TEXT))
-            .arguments(GenericArguments.optional(Arguments.schematic(SCHEMATIC)))
-            .executor(new CommandCreate())
-            .build();
+  public static void register() {
+    CommandSpec commandSpec = CommandSpec.builder()
+        .permission(Permissions.COMMAND_CREATE)
+        .description(Text.of(HELP_TEXT))
+        .arguments(GenericArguments.optional(Arguments.schematic(SCHEMATIC)))
+        .executor(new CommandCreate())
+        .build();
 
-        try {
-            CommandIsland.addSubCommand(commandSpec, "create");
-            PLUGIN.getGame().getCommandManager().register(PLUGIN, commandSpec);
-            PLUGIN.getLogger().debug("Registered command: CommandCreate");
-        } catch (UnsupportedOperationException e) {
-            PLUGIN.getLogger().error("Failed to register command: CommandCreate", e);
-        }
+    try {
+      CommandIsland.addSubCommand(commandSpec, "create");
+      PLUGIN.getGame().getCommandManager().register(PLUGIN, commandSpec);
+      PLUGIN.getLogger().debug("Registered command: CommandCreate");
+    } catch (UnsupportedOperationException e) {
+      PLUGIN.getLogger().error("Failed to register command: CommandCreate", e);
+    }
+  }
+
+  @Override
+  public CommandResult execute(Player player, CommandContext args) throws CommandException {
+    if (Island.hasIsland(player.getUniqueId())) {
+      throw new CommandException(Text.of(TextColors.RED, "You already have an island!"));
+    }
+    boolean canCreate = Options.getMaxIslands(player.getUniqueId()) < 1
+        || Options.getMaxIslands(player.getUniqueId()) - Island.getTotalIslands(player) > 0;
+    if (!canCreate) {
+      throw new CommandException(Text.of(TextColors.RED, "You have reached your maximum number of islands!"));
     }
 
-    @Override public CommandResult execute(Player player, CommandContext args) throws CommandException {
+    Optional<String> schematic = args.getOne(SCHEMATIC);
+    if (schematic.isPresent()) {
+      return createIsland(player, schematic.get());
+    } else if (PLUGIN.getConfig().getMiscConfig().isListSchematics() && SchematicArgument.SCHEMATICS.size() > 1) {
+      return listSchematics(player);
+    } else {
+      return createIsland(player, Options.getDefaultSchematic(player.getUniqueId()));
+    }
+  }
+
+  private CommandResult createIsland(Player player, String schematic) throws CommandException {
+    player.sendMessage(Text.of(
+        TextColors.GREEN, "Your island is being created.",
+        PLUGIN.getConfig().getMiscConfig().isTeleportOnCreate() ? " You will be teleported shortly." : Text.EMPTY
+    ));
+
+    try {
+      new Island(player, schematic);
+      return CommandResult.success();
+    } catch (CreateIslandException e) {
+      throw new CommandException(Text.of(TextColors.RED, "Unable to create island!", Text.NEW_LINE, TextColors.RESET, e.getText()));
+    }
+  }
+
+  private CommandResult listSchematics(Player player) {
+    boolean checkPerms = PLUGIN.getConfig().getPermissionConfig().isSeparateSchematicPerms();
+    List<Text> schematics = SchematicArgument.SCHEMATICS.keySet().stream()
+        .filter(s -> !checkPerms || player.hasPermission(Permissions.COMMAND_ARGUMENTS_SCHEMATICS + "." + s.toLowerCase()))
+        .map(s -> Text.builder(s).onClick(TextActions.executeCallback(createIsland(s))).build())
+        .collect(Collectors.toList());
+    PaginationList.builder()
+        .title(Text.of(TextColors.AQUA, "Starter Islands"))
+        .padding(Text.of(TextColors.AQUA, TextStyles.STRIKETHROUGH, "-"))
+        .contents(schematics)
+        .sendTo(player);
+    return CommandResult.empty();
+  }
+
+  private Consumer<CommandSource> createIsland(String s) {
+    return src -> {
+      if (src instanceof Player) {
+        Player player = (Player) src;
         if (Island.hasIsland(player.getUniqueId())) {
-            throw new CommandException(Text.of(TextColors.RED, "You already have an island!"));
+          player.sendMessage(Text.of(TextColors.RED, "You already have an island!"));
+          return;
         }
-
-        Optional<String> schematic = args.getOne(SCHEMATIC);
-        if (schematic.isPresent()) {
-            return createIsland(player, schematic.get());
-        } else if (PLUGIN.getConfig().getMiscConfig().isListSchematics() && SchematicArgument.SCHEMATICS.size() > 1) {
-            return listSchematics(player);
-        } else {
-            return createIsland(player, Options.getDefaultSchematic(player.getUniqueId()));
-        }
-    }
-
-    private CommandResult createIsland(Player player, String schematic) throws CommandException {
-        player.sendMessage(Text.of(
-            TextColors.GREEN, "Your island is being created.",
-            PLUGIN.getConfig().getMiscConfig().isTeleportOnCreate() ? " You will be teleported shortly." : Text.EMPTY
-        ));
-
         try {
-            new Island(player, schematic);
-            return CommandResult.success();
+          player.sendMessage(Text.of(
+              TextColors.GREEN, "Your island is being created.",
+              PLUGIN.getConfig().getMiscConfig().isTeleportOnCreate() ? " You will be teleported shortly." : Text.EMPTY
+          ));
+          new Island(player, s);
         } catch (CreateIslandException e) {
-            throw new CommandException(Text.of(TextColors.RED, "Unable to create island!", Text.NEW_LINE, TextColors.RESET, e.getText()));
+          player.sendMessage(Text.of(TextColors.RED, "Unable to create island!", Text.NEW_LINE, TextColors.RESET, e.getText()));
         }
-    }
-
-    private CommandResult listSchematics(Player player) {
-        boolean checkPerms = PLUGIN.getConfig().getPermissionConfig().isSeparateSchematicPerms();
-        List<Text> schematics = SchematicArgument.SCHEMATICS.keySet().stream()
-            .filter(s -> !checkPerms || player.hasPermission(Permissions.COMMAND_ARGUMENTS_SCHEMATICS + "." + s.toLowerCase()))
-            .map(s -> Text.builder(s).onClick(TextActions.executeCallback(createIsland(s))).build())
-            .collect(Collectors.toList());
-        PaginationList.builder()
-            .title(Text.of(TextColors.AQUA, "Starter Islands"))
-            .padding(Text.of(TextColors.AQUA, TextStyles.STRIKETHROUGH, "-"))
-            .contents(schematics)
-            .sendTo(player);
-        return CommandResult.empty();
-    }
-
-    private Consumer<CommandSource> createIsland(String s) {
-        return src -> {
-            if (src instanceof Player) {
-                Player player = (Player) src;
-                if (Island.hasIsland(player.getUniqueId())) {
-                    player.sendMessage(Text.of(TextColors.RED, "You already have an island!"));
-                    return;
-                }
-                try {
-                    player.sendMessage(Text.of(
-                        TextColors.GREEN, "Your island is being created.",
-                        PLUGIN.getConfig().getMiscConfig().isTeleportOnCreate() ? " You will be teleported shortly." : Text.EMPTY
-                    ));
-                    new Island(player, s);
-                } catch (CreateIslandException e) {
-                    player.sendMessage(Text.of(TextColors.RED, "Unable to create island!", Text.NEW_LINE, TextColors.RESET, e.getText()));
-                }
-            }
-        };
-    }
+      }
+    };
+  }
 }
--- src/main/java/net/mohron/skyclaims/team/InviteService.java
+++ src/main/java/net/mohron/skyclaims/team/InviteService.java
@@ -21,8 +21,14 @@ package net.mohron.skyclaims.team;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Table;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import net.mohron.skyclaims.SkyClaims;
 import net.mohron.skyclaims.permissions.Options;
+import net.mohron.skyclaims.world.Island;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.User;
@@ -32,141 +38,149 @@ import org.spongepowered.api.text.action.TextActions;
 import org.spongepowered.api.text.format.TextColors;
 import org.spongepowered.api.text.format.TextStyles;
 
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
-
 public class InviteService {
 
-    private static final SkyClaims PLUGIN = SkyClaims.getInstance();
+  private static final SkyClaims PLUGIN = SkyClaims.getInstance();
 
-    private Table<User, User, Invite> invites = HashBasedTable.create();
+  private Table<User, User, Invite> invites = HashBasedTable.create();
 
-    public InviteService() {
+  public InviteService() {
 
-    }
+  }
 
-    private enum Type {
-        INCOMING, OUTGOING;
-    }
+  private enum Type {
+    INCOMING, OUTGOING;
+  }
 
-    void addInvite(Invite invite) {
-        invites.put(invite.getReceiver(), invite.getSender(), invite);
-    }
+  void addInvite(Invite invite) {
+    invites.put(invite.getReceiver(), invite.getSender(), invite);
+  }
 
-    void removeInvite(Invite invite) {
-        invites.remove(invite.getReceiver(), invite.getSender());
-    }
+  void removeInvite(Invite invite) {
+    invites.remove(invite.getReceiver(), invite.getSender());
+  }
 
-    public boolean inviteExists(Invite invite) {
-        return invites.contains(invite.getReceiver(), invite.getSender());
-    }
+  public boolean inviteExists(Invite invite) {
+    return invites.contains(invite.getReceiver(), invite.getSender());
+  }
 
-    public int getInviteCount(User user) {
-        return invites.row(user).values().size();
-    }
+  public int getInviteCount(User user) {
+    return invites.row(user).values().size();
+  }
 
-    public Consumer<CommandSource> listIncomingInvites() {
-        return listInvites(Type.INCOMING);
-    }
+  public Consumer<CommandSource> listIncomingInvites() {
+    return listInvites(Type.INCOMING);
+  }
 
-    public Consumer<CommandSource> listOutgoingInvites() {
-        return listInvites(Type.OUTGOING);
-    }
+  public Consumer<CommandSource> listOutgoingInvites() {
+    return listInvites(Type.OUTGOING);
+  }
 
-    private List<Text> getIncomingInviteText(User user) {
-        SimpleDateFormat sdf = PLUGIN.getConfig().getMiscConfig().getDateFormat();
-        return invites.row(user).values().stream()
-            .map(invite -> Text.of(
-                TextColors.WHITE, "[",
-                Text.builder("✓")
-                    .color(TextColors.GREEN)
-                    .onHover(TextActions.showText(Text.of(TextColors.GREEN, "Accept")))
-                    .onClick(TextActions.executeCallback(src -> invite.accept())),
-                TextColors.WHITE, "] [",
-                Text.builder("✗")
-                    .color(TextColors.RED)
-                    .onHover(TextActions.showText(Text.of(TextColors.RED, "Deny")))
-                    .onClick(TextActions.executeCallback(src -> invite.deny())),
-                TextColors.WHITE, "] ",
-                invite.getPrivilegeType().toText(),
-                TextColors.WHITE, " : ",
-                invite.getIsland().getName().toBuilder()
-                    .onHover(TextActions.showText(Text.of(
-                        TextColors.YELLOW, "Invited by", TextColors.WHITE, " : ", TextColors.GOLD, invite.getSender().getName(), Text.NEW_LINE,
-                        TextColors.YELLOW, "Sent on", TextColors.WHITE, " : ", TextColors.GRAY, sdf.format(Date.from(invite.getSent()))
-                    )))
-            ))
-            .collect(Collectors.toList());
-    }
+  private List<Text> getIncomingInviteText(User user) {
+    SimpleDateFormat sdf = PLUGIN.getConfig().getMiscConfig().getDateFormat();
+    boolean canJoin = Options.getMaxIslands(user.getUniqueId()) < 1 || Options.getMaxIslands(user.getUniqueId()) - Island.getTotalIslands(user) > 0;
+    return invites.row(user).values().stream()
+        .map(invite -> Text.of(
+            TextColors.WHITE, "[",
+            Text.builder("✓")
+                .color(TextColors.GREEN)
+                .onHover(TextActions.showText(Text.of(TextColors.GREEN, "Accept")))
+                .onClick(TextActions.executeCallback(acceptInvite(invite, canJoin))),
+            TextColors.WHITE, "] [",
+            Text.builder("✗")
+                .color(TextColors.RED)
+                .onHover(TextActions.showText(Text.of(TextColors.RED, "Deny")))
+                .onClick(TextActions.executeCallback(src -> invite.deny())),
+            TextColors.WHITE, "] ",
+            invite.getPrivilegeType().toText(),
+            TextColors.WHITE, " : ",
+            invite.getIsland().getName().toBuilder()
+                .onHover(TextActions.showText(Text.of(
+                    TextColors.YELLOW, "Invited by", TextColors.WHITE, " : ", TextColors.GOLD,
+                    invite.getSender().getName(), Text.NEW_LINE,
+                    TextColors.YELLOW, "Sent on", TextColors.WHITE, " : ", TextColors.GRAY,
+                    sdf.format(Date.from(invite.getSent()))
+                )))
+        ))
+        .collect(Collectors.toList());
+  }
 
-    private List<Text> getOutgoingInviteText(User user) {
-        SimpleDateFormat sdf = PLUGIN.getConfig().getMiscConfig().getDateFormat();
-        return invites.column(user).values().stream()
-            .map(invite -> Text.of(
-                TextColors.WHITE, "[",
-                Text.builder("✗")
-                    .color(TextColors.RED)
-                    .onHover(TextActions.showText(Text.of(TextColors.RED, "Cancel")))
-                    .onClick(TextActions.executeCallback(src -> {
-                        invite.deny();
-                        src.sendMessage(Text.of(
-                            TextColors.GREEN, "Invite to ",
-                            invite.getPrivilegeType().format(invite.getReceiver().getName()),
-                            TextColors.GREEN, " has been canceled."
-                        ));
-                    })),
-                TextColors.WHITE, "] ",
-                invite.getPrivilegeType().format(invite.getReceiver().getName()),
-                TextColors.WHITE, " : ",
-                invite.getIsland().getName(),
-                TextColors.WHITE, " : ",
-                TextColors.GRAY, sdf.format(Date.from(invite.getSent()))
-            ))
-            .collect(Collectors.toList());
-    }
+  private Consumer<CommandSource> acceptInvite(Invite invite, boolean canJoin) {
+    return src -> {
+      if (canJoin) {
+        invite.accept();
+      } else {
+        src.sendMessage(Text.of(TextColors.RED, "You have reached your maximum number of islands!"));
+      }
+    };
+  }
 
-    private Consumer<CommandSource> listInvites(Type type) {
-        return src -> {
-            if (src instanceof Player) {
-                Player player = (Player) src;
+  private List<Text> getOutgoingInviteText(User user) {
+    SimpleDateFormat sdf = PLUGIN.getConfig().getMiscConfig().getDateFormat();
+    return invites.column(user).values().stream()
+        .map(invite -> Text.of(
+            TextColors.WHITE, "[",
+            Text.builder("✗")
+                .color(TextColors.RED)
+                .onHover(TextActions.showText(Text.of(TextColors.RED, "Cancel")))
+                .onClick(TextActions.executeCallback(src -> {
+                  invite.deny();
+                  src.sendMessage(Text.of(
+                      TextColors.GREEN, "Invite to ",
+                      invite.getPrivilegeType().format(invite.getReceiver().getName()),
+                      TextColors.GREEN, " has been canceled."
+                  ));
+                })),
+            TextColors.WHITE, "] ",
+            invite.getPrivilegeType().format(invite.getReceiver().getName()),
+            TextColors.WHITE, " : ",
+            invite.getIsland().getName(),
+            TextColors.WHITE, " : ",
+            TextColors.GRAY, sdf.format(Date.from(invite.getSent()))
+        ))
+        .collect(Collectors.toList());
+  }
 
-                Text title = Text.of(
-                    TextColors.AQUA, "Invites : ",
-                    TextColors.AQUA, (type == Type.INCOMING) ? "[" : Text.EMPTY,
-                    Text.builder("Incoming")
-                        .color((type == Type.INCOMING) ? TextColors.GREEN : TextColors.GRAY)
-                        .onHover(TextActions.showText(Text.of("Click here to show ", TextColors.GREEN, "incoming", TextColors.RESET, " invites")))
-                        .onClick(TextActions.executeCallback(listIncomingInvites())),
-                    TextColors.AQUA, (type == Type.INCOMING) ? "] " : " [",
-                    Text.builder("Outgoing")
-                        .color((type == Type.OUTGOING) ? TextColors.YELLOW : TextColors.GRAY)
-                        .onHover(TextActions.showText(Text.of("Click here to show ", TextColors.YELLOW, "outgoing", TextColors.RESET, " invites")))
-                        .onClick(TextActions.executeCallback(listOutgoingInvites())),
-                    TextColors.AQUA, (type == Type.OUTGOING) ? "]" : Text.EMPTY
-                );
+  private Consumer<CommandSource> listInvites(Type type) {
+    return src -> {
+      if (src instanceof Player) {
+        Player player = (Player) src;
 
-                List<Text> contents = (type == Type.INCOMING)
-                    ? PLUGIN.getInviteService().getIncomingInviteText(player)
-                    : PLUGIN.getInviteService().getOutgoingInviteText(player);
-                if (contents.isEmpty()) {
-                    contents = ImmutableList.of(Text.of(TextColors.RED, "You have no ", type.toString().toLowerCase(), " invites!"));
-                }
+        Text title = Text.of(
+            TextColors.AQUA, "Invites : ",
+            TextColors.AQUA, (type == Type.INCOMING) ? "[" : Text.EMPTY,
+            Text.builder("Incoming")
+                .color((type == Type.INCOMING) ? TextColors.GREEN : TextColors.GRAY)
+                .onHover(TextActions.showText(Text.of("Click here to show ", TextColors.GREEN, "incoming", TextColors.RESET, " invites")))
+                .onClick(TextActions.executeCallback(listIncomingInvites())),
+            TextColors.AQUA, (type == Type.INCOMING) ? "] " : " [",
+            Text.builder("Outgoing")
+                .color((type == Type.OUTGOING) ? TextColors.YELLOW : TextColors.GRAY)
+                .onHover(TextActions.showText(Text.of("Click here to show ", TextColors.YELLOW, "outgoing", TextColors.RESET, " invites")))
+                .onClick(TextActions.executeCallback(listOutgoingInvites())),
+            TextColors.AQUA, (type == Type.OUTGOING) ? "]" : Text.EMPTY
+        );
 
-                int limit = Options.getMaxIslands(player.getUniqueId());
-                Text header = (limit < 1)
-                    ? null
-                    : Text.of(TextColors.GRAY, "You may join up to ", TextColors.LIGHT_PURPLE, limit, TextColors.GRAY, " islands.", Text.NEW_LINE);
+        List<Text> contents = (type == Type.INCOMING)
+            ? PLUGIN.getInviteService().getIncomingInviteText(player)
+            : PLUGIN.getInviteService().getOutgoingInviteText(player);
+        if (contents.isEmpty()) {
+          contents = ImmutableList.of(Text.of(TextColors.RED, "You have no ", type.toString().toLowerCase(), " invites!"));
+        }
 
-                PaginationList.builder()
-                    .title(title)
-                    .header(header)
-                    .padding(Text.of(TextColors.AQUA, TextStyles.STRIKETHROUGH, "-"))
-                    .contents(contents)
-                    .sendTo(player);
-            }
-        };
-    }
+        int limit = Options.getMaxIslands(player.getUniqueId());
+        Text header = limit < 1
+            ? null
+            : Text.of(TextColors.GRAY, "You currently have ", TextColors.LIGHT_PURPLE, Island.getTotalIslands(player), TextColors.GRAY, " of ",
+                TextColors.LIGHT_PURPLE, limit, TextColors.GRAY, " maximum island", limit > 1 ? "s" : Text.EMPTY, ".");
+
+        PaginationList.builder()
+            .title(title)
+            .header(header)
+            .padding(Text.of(TextColors.AQUA, TextStyles.STRIKETHROUGH, "-"))
+            .contents(contents)
+            .sendTo(player);
+      }
+    };
+  }
 }
